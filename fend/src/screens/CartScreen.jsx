f39836a5df59dfced9dbda0ad4d80ccd55e19016
Your Name <you@example.com>
2021-09-25 16:42:19 +0530

    almighty Push

--- fend/src/screens/CartScreen.jsx
+++ fend/src/screens/CartScreen.jsx
@@ -24,6 +24,7 @@ const CartScreen = (props) => {
   const checkoutHandler=()=>{
     props.history.push('/signin/?redirect=shipping')
   }
+  console.log('cartScreen',props)
   return (
     <div className="row top">
       <div className="col-2">
